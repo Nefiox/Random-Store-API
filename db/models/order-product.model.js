d436ce70b114d8fbcff2c6d1725c06a70c0a2728
Juan Jiménez <nefioxdev@gmail.com>
2022-01-18 21:18:21 -0600

    Many to many associations

---
+++ db/models/order-product.model.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { ORDER_TABLE } = require('./order.model');
+const { PRODUCT_TABLE } = require('./product.model');
+
+const ORDER_PRODUCT_TABLE = 'orders_products'; // nombre de la tabla
+
+const OrderProductSchema = {
+  // El esquema define la estructura de la BD.
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  amount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  orderId: {
+    field: 'order_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: ORDER_TABLE,
+      key: 'id',
+    },
+  },
+  productId: {
+    field: 'product_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references: {
+      model: PRODUCT_TABLE,
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL',
+  },
+};
+
+class OrderProduct extends Model {
+  static associate(models) {}
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: ORDER_PRODUCT_TABLE,
+      modelName: 'OrderProduct',
+      timestamps: false,
+    };
+  }
+}
+
+module.exports = { ORDER_PRODUCT_TABLE, OrderProductSchema, OrderProduct };
