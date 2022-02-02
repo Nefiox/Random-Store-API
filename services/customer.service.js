67b65f32c52c90fca3f49d648617901b35ad19ae
Juan Jiménez <nefioxdev@gmail.com>
2022-02-01 22:13:16 -0600

    Implementing password hashing

--- services/customer.service.js
+++ services/customer.service.js
@@ -1,4 +1,5 @@
 const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
 const { models } = require('../libs/sequelize');
 
 class CustomerService {
@@ -25,9 +26,18 @@ class CustomerService {
     //   ...data,
     //   userId: newUser.id,
     // });
-    const newCustomer = await models.Customer.create(data, {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    const newData = {
+      ...data,
+      user: {
+        ...data.user,
+        password: hash,
+      },
+    };
+    const newCustomer = await models.Customer.create(newData, {
       include: ['user'],
     });
+    delete newCustomer.user.dataValues.password;
     return newCustomer;
   }
 
